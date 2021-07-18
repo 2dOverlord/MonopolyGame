50f445085ef7f19dbe0b2ca8b82cd0f0c722b908
Vladyslav Kriuchkovskyi <kriuchkovskyi.vlad@gmail.com>
2021-07-19 02:22:03 +0300

    Issue #2: Django simple registration

--- UserApp/urls.py
+++ UserApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.render_main_page),
-    path('user/<int:user_id>/', views.render_user_page, name='user')
+    path('', views.render_main_page, name='main'),
+    path('user/<int:user_id>/', views.render_user_page, name='user'),
+    path('register/', views.render_register_page, name='register')
 ]
--- UserApp/views.py
+++ UserApp/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import CustomUser
 from ItemApp.models import Item
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from .forms import CustomRegistrationForm
 
 def render_main_page(request):
     return render(request, 'main-page/main-page.html')
@@ -13,3 +16,18 @@ def render_user_page(request, user_id = 1):
         "inventory": list(inventory),
     }
     return render(request, 'user-page/user-page.html', data)
+
+def render_register_page(request):
+    context = {}
+    if request.POST:
+        form = CustomRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main')
+        else:
+            print('NOT VALID')
+            context['registration_form'] = form
+    else:
+        form = CustomRegistrationForm()
+        context['registration_form'] = form
+    return render(request, 'register-page/register-page.html', context)
