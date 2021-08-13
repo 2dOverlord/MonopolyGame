0bd43aed20b9feb4828929d56f83227bc0a62e2b
Vladyslav Kriuchkovskyi <kriuchkovskyi.vlad@gmail.com>
2021-08-13 20:50:07 +0300

    Issue #80

--- MarketApp/views.py
+++ MarketApp/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from .models import SellOffer
+from ItemApp.models import Item
+from django.db.models import Count, Min
 
 def render_market_page(request):
-    offers = SellOffer.objects.all()
+    offers = (SellOffer.objects.values('item')
+              .annotate(dcount=Count('item'), min_price=Min('price'))
+              .order_by()
+              )
+
+    for offer in offers:
+        item = Item.get_item_by_id(offer['item'])
+        offer['item_name'] = item.name
+        offer['item_image_url'] = item.image.url
+        offer['item_rarity'] = item.rarity
+
     context = {
         'offers': offers
     }
